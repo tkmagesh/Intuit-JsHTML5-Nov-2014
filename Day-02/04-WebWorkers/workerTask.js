be89536f96dcc92edc7072b00e1c4eaac0d70f5d
tkmagesh <tkmagesh77@gmail.com>
2014-11-28 14:39:31 +0530

    Web Workers Demo & Assignment Seed

---
+++ Day-02/04-WebWorkers/workerTask.js
@@ -0,0 +1,21 @@
+function doWork(){
+	for(var i=0;i<10000;i++){
+		for(var j=0;j<10000;j++)
+			for(var k=0;k<100;k++){
+				
+			}
+		if (i % 100 === 0){
+			var percentCompleted = i / 100;
+			self.postMessage({
+				type : "progress",
+				percentCompleted : percentCompleted
+			});
+		}
+	}
+}
+self.addEventListener("message", function (msgEvt){
+	if (msgEvt.data === "start"){
+		doWork();
+		self.postMessage({type : "completed"});
+	}
+});
